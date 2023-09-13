181c754eea79014382ea40618da0772afd4f20ab
meenachinmay <Chinmayanand896@gmail.com>
2023-09-13 19:26:49 +0900

    fixed docker-compose and Docker and fixed prisma too

---
+++ prisma/migrations/20230913102257_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Message_userId_idx" ON "Message"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,13 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
-import { GoogleOauthGuard } from './google-auth.gaurd';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(GoogleOauthGuard)
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/auth/google')
-  @UseGuards(AuthGuard('google'))
-  async googleAuth(@Req() req) {}
-
-  @Get('auth/google/callback')
-  @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req, @Res() res) {
-    const user = req.user;
-    req.session.user = user;
-
-    res.redirect(`http://localhost:5173/home?user=${JSON.stringify(user)}`);
   }
 }
